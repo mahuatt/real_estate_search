43af91ac70e2eeb79bb69b3880462cf0f18e8172
mahuatt <115101525+mahuatt@users.noreply.github.com>
2024-07-03 10:08:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#数据库连接配置
+import pymysql
+
+conn = pymysql.connect(
+        host='120.46.3.93',
+        port=3306,
+        user='root',
+        password='0411mtxM+',
+        database='user'
+    )
